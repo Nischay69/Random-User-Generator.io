a60190b2356137d22465c936a8fb53ac6e58ede8
Nischay <nischaymaharjann@gmail.com>
2023-08-09 21:03:52 +0545

    Second Commit

--- script.js
+++ script.js
@@ -1,35 +1,36 @@
-function getRandomUser() {
-  let url = "https://randomuser.me/api/";
-  let response = fetch(url);
-  response
-    .then((value) => {
-      return value.json();
-    })
-    .then((value) => {
-      const user = value.results[0];
-      console.log(user);
-      displayUser(user);
-    });
+async function getRandomUser() {
+  const url = "https://randomuser.me/api/";
+  const response = await fetch(url);
+  const value = await response.json();
+  const user = value.results[0];
+  console.log(user);
+  displayUser(user);
 }
 function displayUser(user) {
   const UserImage = document.querySelector(".user-img");
   const name = document.querySelector(".name");
-  const dob = document.querySelector(".dob-text");
-  const country = document.querySelector(".location-text");
-  const number = document.querySelector(".phone-text");
-  const email = document.querySelector(".email-text");
-  const password = document.querySelector(".password-text");
-  const gender = document.querySelector(".gender-text");
+  const dob = document.querySelector(".dob");
+  const country = document.querySelector(".location");
+  const number = document.querySelector(".phone");
+  const email = document.querySelector(".email");
+  const password = document.querySelector(".password");
+  const gender = document.querySelector(".gender");
 
   UserImage.innerHTML = `  <img
   src=${user.picture.large}
   class="user-img"/>`;
   name.innerText = `${user.name.first} ${user.name.last}`;
-  dob.innerText = `${user.dob.date}`;
-  country.innerText = `${user.location.country}`;
-  number.innerText = `${user.cell}`;
-  email.innerText = `${user.email}`;
-  password.innerText = `${user.login.password}`;
-  gender.innerText = `${user.gender}`;
+  dob.innerHTML = `<img src="img/cal.svg" alt="" class="cal" />
+  <div class="dob-text">${user.dob.date}</div>`;
+  gender.innerHTML = ` <img src="img/gender.png" alt="" class="cal" />
+  <div class="gender-text">${user.gender}</div>`;
+  country.innerHTML = `<img src="img/location.png" alt="" class="cal" />
+  <div class="location-text">${user.location.country}</div>`;
+  number.innerHTML = ` <img src="img/phone.png" alt="" class="cal" />
+  <div class="phone-text">${user.cell}</div>`;
+  email.innerHTML = `<img src="img/email.png" alt="" class="cal email-img" />
+  <div class="email-text">${user.email}</div>`;
+  password.innerHTML = `<img src="img/pass.png" alt="" class="cal" />
+  <div class="password-text">${user.login.password}</div>`;
 }
 getRandomUser();
